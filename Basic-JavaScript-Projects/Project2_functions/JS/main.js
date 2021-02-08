92ab68512be55cf23773d61bf74c8a80e92846bf
npranno <77467876+npranno@users.noreply.github.com>
2021-02-08 13:30:41 -0800

    Math Operators

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -1,11 +1,11 @@
 function myFunction1() {
     var str = "This is blue text!";
     var result = str.fontcolor('blue');
-    document.getElementById("blue_text").innerHTML = result;
+    document.getElementById("blue_text").innerHTML = result; //this function is called to create a sentence where the font color is blue, with the string variable sating "This is blue text!"
 }
 
 function myFunction2() {
     var sentence = "I am learning";
     sentence += " a lot from this book!";
-    document.getElementById("Concatenate").innerHTML = sentence;
+    document.getElementById("Concatenate").innerHTML = sentence; //this is a concatenated sentence function that can be clicked on
 }
---
+++ Basic-JavaScript-Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,46 @@
+function additionFunction() {
+    var addition = 2 + 2;
+    document.getElementById("Math1").innerHTML = "2 + 2 = " + addition; //this uses the variable 'addition' and assigns the id as 'Math1', then when called by the HTML it displays "2 + 2 =" and then displays the value of the addition variable.
+}
+
+function subtractionFunction() {
+    var subtraction = 5 - 3;
+    document.getElementById("Math2").innerHTML = "5 - 3 = " + subtraction; //this uses the variable 'subtraction' and assigns the id as 'Math2', then when called by the HTML it displays "5 - 3 =" and then displays the value of the subtraction variable.
+}
+
+function multiplicationFunction() {
+    var multiply = 5 * 3;
+    document.getElementById("Math3").innerHTML = "5 * 3 = " + multiply;
+}
+
+function divisionFunction() {
+    var divide = 20/5;
+    document.getElementById("Math4").innerHTML = "20 / 5 = " + divide;
+}
+
+function multipleOperations() {
+    var simple_Math = (1 + 2) * 10 / 2 - 5;
+    document.getElementById('Math5').innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracated by 5 equals " + simple_Math;
+}
+
+function modulusOperator() {
+    var modulus = 25 % 6;
+    document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a remainder of " + modulus;
+}
+
+function negotiationOperator() {
+    var x = 10;
+    document.getElementById("Math7").innerHTML = "The opposite of 10 is " + -x;
+}
+
+var X = 10;
+X++;
+document.write(X);
+
+var Y = 15;
+Y--;
+document.write(Y);
+
+window.alert(Math.random() * 100);
+
+document.write(Math.random() * 50);
